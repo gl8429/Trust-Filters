4227d93adef67eeb9e8e04165f18603c5417dbcf
gl8429 <lucifer92.lin@gmail.com>
2016-04-19 21:43:57 -0500

    update code by adding output stream, add vectors and train dataset

--- Code/preprocessNER.py
+++ Code/preprocessNER.py
@@ -8,23 +8,10 @@ os.environ["STANFORD_MODELS"] = "/Users/Lucifer/Documents/GraduateStudy/NLP/Trus
 
 #st1 = st.StanfordNERTagger('english.all.3class.distsim.crf.ser.gz')
 #st2 = st.StanfordNERTagger('english.conll.4class.distsim.crf.ser.gz')
-st = st.StanfordNERTagger('english.muc.7class.distsim.crf.ser.gz')
+st3 = st.StanfordNERTagger('english.muc.7class.distsim.crf.ser.gz')
 
+#Labelled data
 fileName = "train.txt"
-with open(fileName, 'r') as f:
-    sentences = []
-    for line in f:
-        strings = st3.tag(line.split())
-        line = [retrieve(strings)] + line.split()
-        sentences.append(line)
-
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-model = gensim.models.Word2Vec(sentences, size=100, window=5, min_count=1, workers=4)
-model.save('myModel')
-# new_model = gensim.models.Word2Vec.load('myModel')
-
-model.init_sims(replace=True)
-
 
 #retrieve named entity recognizer information from tagged sentences, and combine them.
 def retrieve(strings):
@@ -33,3 +20,31 @@ def retrieve(strings):
         if string[1] != 'O':
             res += string[0] + "_"
     return res[:len(res) - 1]
+
+with open(fileName, 'r') as fileInput:
+    sentences = []
+    for sentence in fileInput:
+        words = sentence.split()
+        strings = st3.tag(words)
+        sentence = [words[0]] + [retrieve(strings)] + words[1:]
+        sentences.append(sentence)
+fileInput.close()
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+model = gensim.models.Word2Vec(sentences, size=100, window=5, min_count=1, workers=4)
+model.save('myModel')
+# new_model = gensim.models.Word2Vec.load('myModel')
+
+# Output our results into file
+fileOutput = open('test.txt', 'w')
+for sentence in sentences:
+    for word in sentence:
+        stringArray = []
+        for num in model[word]:
+            stringArray.append('{:2s}'.format(str(num)))
+        fileOutput.write(" ".join(stringArray))
+        fileOutput.write('\n')
+    fileOutput.write("\n\n")
+fileOutput.close()
+
+model.init_sims(replace=True)
