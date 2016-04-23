79018d90b9957a95bad6bbb8e66f08ff92f586f1
zhhsplendid <zhhsplendid@gmail.com>
2016-04-23 09:48:55 -0500

    add run.sh to output results to files

---
+++ Code/run.sh
@@ -0,0 +1,7 @@
+
+
+python process_data.py raw_data/train5500.txt raw_data/test.txt -type
+
+THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python conv_net_sentence.py -static -word2vec > output/output.txt
+
+
